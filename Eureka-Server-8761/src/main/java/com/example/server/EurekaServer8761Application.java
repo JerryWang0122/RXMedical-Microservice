62e573eee085df2d5856d65a65b9dfb93739abe0
JerryWang0122 <school832@gmail.com>
2024-06-16 21:28:16 +0800

    eureka server project init

---
+++ Eureka-Server-8761/src/main/java/com/example/server/EurekaServer8761Application.java
@@ -0,0 +1,13 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer8761Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer8761Application.class, args);
+    }
+
+}
