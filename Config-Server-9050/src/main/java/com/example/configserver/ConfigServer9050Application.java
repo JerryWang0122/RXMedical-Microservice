b50fd7acd36b492b1994d677409117ae88d319e5
JerryWang0122 <school832@gmail.com>
2024-06-17 21:34:13 +0800

    project init

---
+++ Config-Server-9050/src/main/java/com/example/configserver/ConfigServer9050Application.java
@@ -0,0 +1,13 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer9050Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer9050Application.class, args);
+    }
+
+}
