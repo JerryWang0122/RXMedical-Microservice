47c6a56503eff3d249da9f4f6cff8529577b59e0
JerryWang0122 <school832@gmail.com>
2024-06-18 09:34:52 +0800

    Project init

---
+++ Gateway-Server-8080/src/main/java/com/example/gatewayserver/GatewayServer8080Application.java
@@ -0,0 +1,13 @@
+package com.example.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServer8080Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServer8080Application.class, args);
+    }
+
+}
