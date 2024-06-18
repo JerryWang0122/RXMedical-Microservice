88e5962db7fc4ffaf0f44466dd079089cdaa71b6
JerryWang0122 <school832@gmail.com>
2024-06-18 10:01:54 +0800

    project init

---
+++ JWT-Service-9000/src/main/java/com/rxmedical/api/JwtService9000Application.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtService9000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtService9000Application.class, args);
+    }
+
+}
