b9ca5a403d1af0de7ba195adcbdb23bf88736da6
JerryWang0122 <school832@gmail.com>
2024-06-18 11:37:34 +0800

    feat: jwtService

---
+++ JWT-Service-9000/src/main/java/com/rxmedical/api/controller/JWTController.java
@@ -0,0 +1,66 @@
+package com.rxmedical.api.controller;
+
+import com.nimbusds.jose.JOSEException;
+import com.rxmedical.api.model.po.User;
+import com.rxmedical.api.model.response.ApiResponse;
+import com.rxmedical.api.service.JWTService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/jwt")
+public class JWTController {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @GetMapping("/test")
+    public String test() {
+        return "JWT test";
+    }
+
+    // 取得CSRFToken
+    @GetMapping("/CSRFToken")
+    public ResponseEntity<ApiResponse<List<String>>> getCSRFToken() throws JOSEException {
+        return ResponseEntity.ok(new ApiResponse<>(true, "CSRFToken", jwtService.getCSRFTokenJWT()));
+    }
+
+    // 驗證CSRFToken
+    @PostMapping("/CSRFToken")
+    public ResponseEntity<ApiResponse<Object>> checkCSRFToken(@RequestBody String token, HttpServletRequest request) throws ParseException {
+        System.out.println("Verify Token" + token);
+        System.out.println(request.getHeader("Authorization"));
+        boolean result = jwtService.checkCSRFTokenJWT(token, request);
+        System.out.println(result);
+        if (result) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "CSRF Token 驗證通過", null));
+        } else {
+            return ResponseEntity.ok(new ApiResponse<>(false, "CSRF Token 驗證失敗", null));
+        }
+    }
+
+    // 取得使用者權限Token
+    @GetMapping("/userToken")
+    public ResponseEntity<ApiResponse<String>> getUserUsageToken(@RequestBody User user) throws JOSEException {
+        return ResponseEntity.ok(new ApiResponse<>(true, "User Token", jwtService.getUserUsageJWT(user)));
+    }
+
+    // 驗證並提取使用者Token資料
+    @PostMapping("/userToken")
+    public ResponseEntity<ApiResponse<Map<String, Object>>> verifyUserUsageToken(HttpServletRequest request) throws ParseException {
+        Map<String, Object> userInfo = jwtService.verifyUserUsageJWT(request);
+        if (userInfo == null) {
+            return ResponseEntity.ok(new ApiResponse<>(false, "User Token 驗證失敗", null));
+        } else {
+            return ResponseEntity.ok(new ApiResponse<>(true, "User Token 驗證通過", userInfo));
+        }
+    }
+
+}
---
+++ JWT-Service-9000/src/main/java/com/rxmedical/api/service/JWTService.java
@@ -0,0 +1,132 @@
+package com.rxmedical.api.service;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.rxmedical.api.model.po.User;
+import com.rxmedical.api.util.KeyUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class JWTService {
+
+    private final static String SIGNING_SECURE_PATH = "./signingSecure.txt";
+    private final static String SIGNING_SECURE = getJWTSigningSecure();
+
+    /**
+     * 使用者在登入成功後發的JWT
+     * @param user 使用者資訊
+     * @return 依照傳入參數產生的JWT，包含id, authLevel
+     * @throws JOSEException
+     */
+    public String getUserUsageJWT(User user) throws JOSEException {
+        if ("register".equals(user.getAuthLevel()) || "off".equals(user.getAuthLevel())) {
+            return null;
+        }
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("User usage")
+                .issuer("RXMedical")
+                .claim("id", user.getId())
+                .claim("authLevel", user.getAuthLevel())
+                .build();
+        return KeyUtil.signJWT(claimsSet, SIGNING_SECURE);
+    }
+
+    public Map<String, Object> verifyUserUsageJWT(HttpServletRequest request) throws ParseException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ") ) {
+            return null;
+        }
+
+        // CSRF 驗證
+        String jwt = authorizationHeader.substring(7);
+        if (!KeyUtil.verifyJWTSignature(jwt, SIGNING_SECURE)) {
+            return null;
+        }
+        JWTClaimsSet claims = KeyUtil.getClaimsFromToken(jwt);
+        return Map.of(
+                "userId", claims.getIntegerClaim("id"),
+                "authLevel", claims.getClaim("authLevel")
+        );
+    }
+
+
+    /**
+     * 取得CSRF Token 及其 JWT
+     * @return [CSRF_Token, JWT(CSRF_Token)]
+     * @throws JOSEException
+     */
+    public List<String> getCSRFTokenJWT() throws JOSEException {
+        String CSRFToken = getUUID();
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("CSRFToken")
+                .issuer("RXMedical")
+                .claim("CSRFToken", CSRFToken)
+                .build();
+        return List.of(CSRFToken, KeyUtil.signJWT(claimsSet, SIGNING_SECURE));
+    }
+
+    /**
+     * 檢驗request header 的 CSRF Token 與傳入的token是否吻合
+     * @param token form body 傳入的 CSRF Token
+     * @param request header's authorization should be set and bearer token
+     * @return CSRF Token是否符合
+     * @throws ParseException
+     */
+    public boolean checkCSRFTokenJWT(String token, HttpServletRequest request) throws ParseException {
+        if (token == null || request == null) {
+            return false;
+        }
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ") ) {
+            return false;
+        }
+
+        // CSRF 驗證
+        String jwt = authorizationHeader.substring(7);
+        if (!KeyUtil.verifyJWTSignature(jwt, SIGNING_SECURE)) {
+            return false;
+        }
+        JWTClaimsSet claims = KeyUtil.getClaimsFromToken(jwt);
+
+        return token.equals(claims.getClaim("CSRFToken"));
+    }
+
+    /**
+     * [工具] 輔助JWT產生
+     * @return 取得JWT簽名專用密鑰
+     */
+    private static String getJWTSigningSecure() {
+        // 如果檔案存在，則直接取出內容，否則建立並寫入檔案
+        try {
+            if (new File(SIGNING_SECURE_PATH).exists()) {
+                return Files.readString(Path.of(SIGNING_SECURE_PATH));
+            } else {
+                String signingSecret = KeyUtil.generateSecret(32);
+                Files.writeString(Path.of(SIGNING_SECURE_PATH), signingSecret);
+                return signingSecret;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 取得 UUID Token
+     * @return UUID Token
+     */
+    private String getUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
