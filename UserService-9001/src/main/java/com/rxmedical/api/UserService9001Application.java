b0285d548f2e9e5bfd9a5bfd4540cdcdce0b2e36
JerryWang0122 <school832@gmail.com>
2024-06-18 12:26:16 +0800

    project init

---
+++ UserService-9001/src/main/java/com/rxmedical/api/UserService9001Application.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserService9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService9001Application.class, args);
+    }
+
+}
---
+++ UserService-9001/src/test/java/com/rxmedical/api/UserService9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserService9001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
