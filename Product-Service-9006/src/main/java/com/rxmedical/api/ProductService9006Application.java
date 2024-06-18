0704c2983a0f492147c61117d15edcb8b38cd5a0
JerryWang0122 <school832@gmail.com>
2024-06-18 21:35:05 +0800

    Project init

---
+++ Product-Service-9006/src/main/java/com/rxmedical/api/ProductService9006Application.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductService9006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductService9006Application.class, args);
+    }
+
+}
