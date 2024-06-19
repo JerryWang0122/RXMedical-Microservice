f244fd5823d107bb61e12637c34936a6287bfd7b
JerryWang0122 <school832@gmail.com>
2024-06-19 10:03:37 +0800

    Project init

---
+++ Sales-Service-9011/src/main/java/com/rxmedical/api/SalesService9011Application.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesService9011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesService9011Application.class, args);
+    }
+
+}
---
+++ Sales-Service-9011/src/test/java/com/rxmedical/api/SalesService9011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxmedical.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesService9011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
